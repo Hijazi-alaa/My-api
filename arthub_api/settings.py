e7604f6c4efcd35bd6b0fee216c98062cae5ad72
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-20 16:25:15 +0000

    Create the likes app and added its serialzers, views and urls

--- arthub_api/settings.py
+++ arthub_api/settings.py
@@ -110,6 +110,7 @@ INSTALLED_APPS = [
     'profiles',
     'posts',
     'comments',
+    'likes',
 ]
 
 SITE_ID = 1
---
+++ likes/serializers.py
@@ -0,0 +1,29 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        """
+        meta class for the LikeSerializer
+        """
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        """
+        create method to return a complete object instance
+        based on the validated data
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
