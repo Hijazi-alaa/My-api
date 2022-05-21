4f4b9055ece3b2f898c8cc2e6c88e04591fb820b
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-21 11:30:55 +0000

    Add like_id to post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from likes.models import Like
 from .models import Post
+
 
 class PostsSerializer(serializers.ModelSerializer):
     """
@@ -9,9 +11,12 @@ class PostsSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
+        """
+        Method to validate acceptable image sizes
+        """
         if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError(
                 "Image size larger than 2MB"
@@ -35,10 +40,26 @@ class PostsSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        """
+        Get method to register each like with its own id
+        """
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
+        """
+        Meta class for the postsserializer class
+        """
         model = Post
         fields = [
             "id", "owner", "is_owner", "profile_id",
             "profile_image", "created_at", "updated_at",
-            "title", "description", "image", "image_filter"
+            "title", "description", "image", "image_filter",
+            "like_id",
         ]
